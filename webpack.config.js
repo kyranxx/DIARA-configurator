d0955ed9259872431095bf2b533073c24342e4f0
kyranxx <blanarikdaniel@gmail.com>
2025-02-10 22:48:04 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+
+module.exports = {
+  entry: './frontend/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@': path.resolve(__dirname, 'frontend')
+    }
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
+  }
+};
