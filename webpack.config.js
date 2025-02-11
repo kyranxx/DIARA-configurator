c46427e8404efe5854690512102d31dcb09ee4a1
Cline Assistant <cline-assistant@example.com>
2025-02-11 15:38:07 +0100

    fix: update vercel and webpack config for proper static deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './frontend/index.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/'
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'static/js/[name].[contenthash:8].js',
+    publicPath: '/',
+    clean: true
   },
   module: {
     rules: [
@@ -18,7 +20,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/media/[name].[hash:8][ext]'
+        }
       },
       {
         test: /\.css$/,
@@ -29,6 +34,12 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx']
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      inject: true
+    })
+  ],
   devServer: {
     static: {
       directory: path.join(__dirname, 'public'),
